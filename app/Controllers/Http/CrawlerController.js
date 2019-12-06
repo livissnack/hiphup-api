973a64849b90e60394e8f8b9913d991e7e5a3d10
guan xin <guanxin@tinytiger.cn>
2019-12-06 16:41:00 +0800

    增加爬取湖北省政府官网要闻新闻

--- app/Controllers/Http/CrawlerController.js
+++ app/Controllers/Http/CrawlerController.js
@@ -19,6 +19,7 @@ class CrawlerController {
         ['yahoo', 'https://www.yahoo.com/news'],
         ['apnews', 'https://www.apnews.com'],
         ['qichun', 'http://www.qichun.gov.cn/'],
+        ['hubei', 'http://www.hubei.gov.cn/'],
         ['default', 'http://news.baidu.com']
       ]);
 
@@ -136,6 +137,22 @@ class CrawlerController {
             });
             return texts;
           }, QICHUN_SELECTED);
+          break;
+
+        case 'hubei':
+          const HUBEI_SELECTED = '.news_list li .news_title';
+          list = await page.evaluate(HUBEI_SELECTED => {
+            let elements = Array.from(
+              document.querySelectorAll(HUBEI_SELECTED)
+            );
+            let texts = elements.map(a => {
+              return {
+                url: a.href.trim(),
+                title: a.innerText
+              };
+            });
+            return texts;
+          }, HUBEI_SELECTED);
           break;
 
         default:
