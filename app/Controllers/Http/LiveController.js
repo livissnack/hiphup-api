5c54766280fe498e3a1e8a4d35f864dc8a999fd8
guan xin <guanxin@tinytiger.cn>
2020-07-31 09:33:08 +0800

    修改虎牙爬源

--- app/Controllers/Http/LiveController.js
+++ app/Controllers/Http/LiveController.js
@@ -40,7 +40,7 @@ class LiveController {
       {
         'Content-Type': 'application/x-www-form-urlencoded',
         'User-Agent':
-          'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Mobile Safari/537.36',
+          'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
       }
     );
     const url = /liveLineUrl = "(.*)"/.exec(data);
